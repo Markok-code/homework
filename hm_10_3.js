a7f882434c9d7aaaf63ca85439816c1ba1885313
Markok-code <gospodinkomaksim4@gmail.com>
2024-09-03 18:03:54 +0300

    Add files via upload

---
+++ hm_10_3.js
@@ -0,0 +1,25 @@
+const contactBook = {
+    contacts: [
+        {
+            name: "John Doe",
+            phone: "+1111111111",
+            email: "johndoe@gmail.com"
+        },
+        {
+            name: "Jane Smith",
+            phone: "+222222222",
+            email: "janesmith@gmail.com"
+        }
+    ],
+
+    findContactByName: function(name) {
+        return this.contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase());
+    },
+    
+    addContact: function(name, phone, email) {
+        this.contacts.push({ name, phone, email });
+    }
+};
+
+
+
